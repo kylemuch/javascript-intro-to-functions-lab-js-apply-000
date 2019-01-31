a8030af30cf3731752582ffb0d267a06db8cfcdb
Kyle <kylemuch@gmail.com>
2019-01-31 04:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var sayHiToGrandma = function(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
   } else if (string === string.toUpperCase()) {
-    return "YES INDEED"
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
