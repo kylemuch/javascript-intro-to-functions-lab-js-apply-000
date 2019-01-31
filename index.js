f98a92f9272ab2a2dc983116d3b295d6290cf5fd
Kyle <kylemuch@gmail.com>
2019-01-31 04:07:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 var shout = function(string) {
   return string.toUpperCase();
 }
+
+var whisper = function(string) {
+  return string.toLowerCase();
+}
+
+var logShout = function(string) {
+  console.log(string.toUpperCase());
+}
+
+var sayHiToGrandma = function(string) {
+  if (string != string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
+
+
